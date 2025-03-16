c42108368fb4815d1dc9d581ce7a8bf3d3da9116
roman.ovcharenko <roman.ovcharenko@everymatrix.com>
2025-03-16 17:55:57 +0200

    next js app init

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -2,11 +2,16 @@ import {ITodoProps, Todo} from '@/app/components/Todo';
 
 const Todos = async () => {
     const getTodos = async () => {
+        await new Promise<void>((resolve): void => {
+            setTimeout(resolve, 5000);
+        })
         const response = await fetch('https://jsonplaceholder.typicode.com/todos');
 
         return await response.json()
     }
     const todos: ITodoProps[] = await getTodos();
+
+    console.log(todos)
 
     return todos.map((todo) => (
         <Todo key={todo.id} {...todo} />
