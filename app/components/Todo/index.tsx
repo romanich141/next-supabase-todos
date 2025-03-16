33c074f18fab8566ebf1bd3112be0c94a8e60788
roman.ovcharenko <roman.ovcharenko@everymatrix.com>
2025-03-16 17:52:10 +0200

    next js app init

--- app/components/Todo/index.tsx
+++ app/components/Todo/index.tsx
@@ -2,11 +2,11 @@ export interface ITodoProps {
     completed: boolean;
     id: number;
     title: string;
-    userId: number;
 }
 
-export const Todo = ({completed, userId, id, title}: ITodoProps) => {
+export const Todo = ({completed,  id, title}: ITodoProps) => {
     return <div className="flex mb-4 items-center">
+            <p className="w-full text-grey-darkest">{id}</p>
             <p className="w-full text-grey-darkest">{title}</p>
             <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green">{completed ? 'Done' : 'Not done'}</button>
         </div>
